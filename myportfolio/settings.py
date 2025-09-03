2d117fe071bebc64d662889110e46071c2f5f15d
vicky <vickyvicky54935@gmail.com>
2025-09-03 20:08:08 +0530

    newtst

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -66,8 +66,7 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+        os.environ.get("DATABASE_URL")
     )
 }
 
